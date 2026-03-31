fa93a40a125ef9492d08a7dadaeba8621ffd8b03
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-03-31 19:45:15 -0300

    ai service: listening to message in rabbitmq queue

---
+++ ai-service/src/main/java/com/fitmedia/aiservice/model/Activity.java
@@ -0,0 +1,23 @@
+package com.fitmedia.aiservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+public class Activity {
+
+    private Long id;
+    private Long userId;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrics = new HashMap<>();
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
